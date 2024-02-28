fc7d905201899e519ccd39e4df90e55a634291aa
Javier Bullrich <javier@bullrich.dev>
2024-02-28 12:55:19 +0100

    added logic to extract rss feed

---
+++ src/rss.ts
@@ -0,0 +1,82 @@
+import { writeFile } from "fs/promises";
+import xml from "xml";
+
+type Post = { title: string; date: string; content: string; link: string };
+
+function buildFeed(posts: Post[]) {
+    const sortedPosts = posts.sort(
+        (first, second) =>
+            new Date(second.date).getTime() - new Date(first.date).getTime(),
+    );
+
+    const feedItems = [];
+
+    feedItems.push(
+        ...sortedPosts.map((post) => {
+            return {
+                item: [
+                    { title: post.title },
+                    {
+                        pubDate: new Date(post.date as string).toUTCString(),
+                    },
+                    {
+                        guid: [
+                            { _attr: { isPermaLink: true } },
+                            post.link,
+                        ],
+                    },
+                    {
+                        description: {
+                            _cdata: post.content,
+                        },
+                    },
+                ],
+            };
+        }),
+    );
+
+    return feedItems;
+}
+
+export const writeRssFeed = async (posts: Post[]) => {
+    console.log("creating feed");
+    const feedObject = {
+        rss: [
+            {
+                _attr: {
+                    version: "2.0",
+                    "xmlns:atom": "http://www.w3.org/2005/Atom",
+                },
+            },
+            {
+                channel: [
+                    {
+                        "atom:link": {
+                            _attr: {
+                                // TODO: Change this
+                                href: "YOUR-WEBSITE/feed.rss",
+                                rel: "self",
+                                type: "application/rss+xml",
+                            },
+                        },
+                    },
+                    {
+                        title: "TLDR RSS Feed",
+                    },
+                    {
+                        // TODO: Change this
+                        link: "YOUR-WEBSITE/",
+                    },
+                    { description: "TLDR RSS Feed" },
+                    { language: "en-US" },
+                    // todo: add the feed items here
+                    ...buildFeed(posts),
+                ],
+            },
+        ],
+    };
+
+    const feed = '<?xml version="1.0" encoding="UTF-8"?>' + xml(feedObject);
+
+    await writeFile("./feed.rss", feed, "utf8");
+};
